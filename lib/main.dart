d761e5925f51b97cdd7ad59a0bc19114372acb90
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-05-30 06:18:02 +0500

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,46 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
+ProductControllers productControllers = ProductControllers();
+
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  ProductControllers productControllers = ProductControllers();
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SecondPage());
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => FirstPage(productControllers: productControllers)));
+                },
+                child: const Text("First page")),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondPage()));
+                },
+                child: const Text("Secon page")),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => FirstPage(productControllers: productControllers)));
+                },
+                child: const Text("First page"))
+          ],
+        ),
+      ),
+    );
   }
 }
